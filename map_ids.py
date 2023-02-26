ab71078d7de08da3761d7cd329e1540ae7a15f57
Tracy Snell <tjs@juicelabs.com>
2023-02-26 18:42:23 -0500

    moved mappings into retailers (and into db_utils)

--- map_ids.py
+++ map_ids.py
@@ -6,22 +6,10 @@ from psycopg.rows import dict_row
 config = load_config('etc/config.yml')
 retailer :RetailerDC = get_retailer_by_key(2)
 
-def load_mappings(retailer_key):
-    sql = "select * from tjs_retailer_ids_mapping where retailer_key = %s"
-    cur = retailer.postgres.cursor(row_factory=dict_row)
-    cur.execute(sql, (retailer_key,))
-    result = []
-    for row in cur:
-        result.append(dict(row.items()))
-    return result
-
-
-def get_mapped_values(mappings, doc):
+def get_mapped_values(doc):
     values = []
-    for mapping in mappings:
-        name = mapping['name']
-        value = doc[mapping['path']]
-        values.append((name, value))
+    for name, value in retailer.id_mappings.items():
+        values.append((name, doc[value]))
     return values
 
 
@@ -36,17 +24,19 @@ def save_id_map(retailer_key, values, model_number, conn):
 
 
 def map_ids():
-    mappings = load_mappings(2)
-    cursor = retailer.mongo_scrape.find({})
+    count = retailer.mongo_scrape.count_documents({})
+    cursor = retailer.mongo_scrape.find()
     conn = get_postgres_connection()
     i = 0
     for document in cursor:
         i += 1
         doc = Dict(document)
         model_number = doc[retailer.model_key]
-        values = get_mapped_values(mappings, doc)
-
+        values = get_mapped_values(doc)
+        if len(values) < 3:
+            pass
         save_id_map(2, values, model_number, conn)
         print(f'saving: {model_number}')
+    print(f'Updated {i} records')
 
 map_ids()
